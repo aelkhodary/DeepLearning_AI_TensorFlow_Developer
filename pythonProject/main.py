a3bf504c98f084fd47fb5d45f51956350390ea10
aelkhodary <40409487+aelkhodary@users.noreply.github.com>
2021-01-03 01:00:14 +0400

    fisrt week

---
+++ pythonProject/main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('------------------PyCharm-----------')
+
+
+
+
+'''
+The Hello World of Deep Learning with Neural Networks
+'''
+def deep_learning_hello_world ():
+
+
+
